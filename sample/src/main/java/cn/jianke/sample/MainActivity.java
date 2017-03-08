8bb7c7c9a2cbd9ce27d5a027cd53c35d7ad1055f
雷兵 <leibing@jianke.com>
2017-03-08 12:00:32 +0800

    Update_modify

--- sample/src/main/java/cn/jianke/sample/MainActivity.java
+++ sample/src/main/java/cn/jianke/sample/MainActivity.java
@@ -1,7 +1,12 @@
 package cn.jianke.sample;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import cn.jianke.liba.StringUtil;
+import cn.jianke.libb.DensityUtil;
+import cn.jianke.libc.PackageUtil;
+
 /**
  * @className: MainActivity
  * @classDescription: 测试组件化页面
@@ -9,10 +14,18 @@ import android.os.Bundle;
  * @createTime: 2017/3/7
  */
 public class MainActivity extends AppCompatActivity {
+    // log tag
+    private final static String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // printer liba class method
+        Log.e(TAG, "--StringUtil#" + StringUtil.getObject(MainActivity.class.getName()));
+        // printer libb class method
+        Log.e(TAG, "--DensityUtil#" + DensityUtil.dip2px(this, 10));
+        // printer libb class method
+        Log.e(TAG, "--PackageUtil#" + PackageUtil.isQQClientAvailable(this));
     }
 }
